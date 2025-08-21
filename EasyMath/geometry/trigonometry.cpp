d110d4bd9bec8e907cf0511e42de461e0ef77085
David <david.tuaillon68@gmail.com>
2025-08-21 13:46:44 +0200

    typo

---
+++ EasyMath/geometry/trigonometry.cpp
@@ -0,0 +1,26 @@
+#include "trigonometry.h"
+
+double EasyMathTrigonometry::degToRad(int deg)
+{
+	return (deg * EasyMathTrigonometry::pi/180);
+}
+
+int EasyMathTrigonometry::radToDeg(int rad)
+{
+	return (rad * 180/EasyMathTrigonometry::pi);
+}
+
+double EasyMathTrigonometry::sin(int x)
+{
+	return 0.0;
+}
+
+double EasyMathTrigonometry::cos(int x)
+{
+	return 0.0;
+}
+
+double EasyMathTrigonometry::tan(int x)
+{
+	return 0.0;
+}
